25e0ff5c5f618e7fc0c66a1361335314a95c1663
paimdev <bathkelucas@gmail.com>
2021-11-30 11:59:37 -0300

    Finish second exercise

--- modulo1/operadores/template_javascript/index.js
+++ modulo1/operadores/template_javascript/index.js
@@ -1,22 +1,33 @@
 // Exercicios de interpretacao
 //
 // Exercicio 1:
-const bool1 = true
-const bool2 = false
-const bool3 = !bool2
+const bool1 = true;
+const bool2 = false;
+const bool3 = !bool2;
 
-let resultado = bool1 && bool2
-console.log("a. ", resultado)
+let resultado = bool1 && bool2;
+console.log("a. ", resultado);
 
-resultado = bool1 && bool2 && bool3 
-console.log("b. ", resultado) 
+resultado = bool1 && bool2 && bool3;
+console.log("b. ", resultado);
 
-resultado = !resultado && (bool1 || bool2) 
-console.log("c. ", resultado)
+resultado = !resultado && (bool1 || bool2);
+console.log("c. ", resultado);
 
-console.log("d. ", typeof resultado)
+console.log("d. ", typeof resultado);
 
 // a. false
 // b. false
 // c. true
 // d. boolean
+
+// Exercicio 2:
+let primeiroNumero = prompt("Digite um numero!");
+let segundoNumero = prompt("Digite outro numero!");
+
+const soma = primeiroNumero + segundoNumero;
+
+console.log(soma);
+
+// Sera impresso o primeiro numero em forma de string concatenado ao segundo
+// Para se resolver use Number()

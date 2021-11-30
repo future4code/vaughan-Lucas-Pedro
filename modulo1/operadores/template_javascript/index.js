8e579d596040dcbe7a6e968c2dc7124b67eb5b38
paimdev <bathkelucas@gmail.com>
2021-11-30 11:57:32 -0300

    First exercise solved

---
+++ modulo1/operadores/template_javascript/index.js
@@ -0,0 +1,22 @@
+// Exercicios de interpretacao
+//
+// Exercicio 1:
+const bool1 = true
+const bool2 = false
+const bool3 = !bool2
+
+let resultado = bool1 && bool2
+console.log("a. ", resultado)
+
+resultado = bool1 && bool2 && bool3 
+console.log("b. ", resultado) 
+
+resultado = !resultado && (bool1 || bool2) 
+console.log("c. ", resultado)
+
+console.log("d. ", typeof resultado)
+
+// a. false
+// b. false
+// c. true
+// d. boolean

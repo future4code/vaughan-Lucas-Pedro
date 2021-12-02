43f4725d4d214e84b72a096510f96727d07a31ca
paimdev <bathkelucas@gmail.com>
2021-12-02 12:32:01 -0300

    Finish second exercise

--- modulo1/funcoes/template_javascript/index.js
+++ modulo1/funcoes/template_javascript/index.js
@@ -7,5 +7,21 @@ function minhaFuncao(variavel) {
 console.log(minhaFuncao(2))
 console.log(minhaFuncao(10))
 
-//10
-//50
+// 10 e 50
+// nada
+
+// 2:
+let textoDoUsuario = prompt("Insira um texto");
+
+const outraFuncao = function(texto) {
+	return texto.toLowerCase().includes("cenoura")
+}
+
+const resposta = outraFuncao(textoDoUsuario)
+console.log(resposta)
+
+// A function retorna o texto em minusculo e diz se existe a palavra cenoura.
+// i. eu gost de cenoura true
+// ii. cenoura e bom pra vista true
+// iii. cenouras crescem na terra true
+

6e173d7f8640e940886ef1933e735719d2f970e2
paimdev <bathkelucas@gmail.com>
2021-12-02 12:54:12 -0300

    Finish third coding exercise

--- modulo1/funcoes/template_javascript/index.js
+++ modulo1/funcoes/template_javascript/index.js
@@ -46,4 +46,23 @@ const evenChecker = (num) => num % 2 === 0;
 const sizeCheckerAndUpper = (string) => {
   console.log(string.toUpperCase);
   console.log(string.length);
-}
+};
+
+// 3:
+const oppSum = (num1, num2) => num1 + num2;
+const oppSub = (num1, num2) => num1 - num2;
+const oppMult = (num1, num2) => num1 * num2;
+const oppDiv = (num1, num2) => num1 / num2;
+
+const numberInput1 = Number(prompt("Digite um numero:"));
+const numberInput2 = Number(prompt("Digite outro numero:"));
+
+const printAnswers = (num1, num2) => {
+  console.log(`Numeros inseridos: ${num1} e ${num2}`);
+  console.log(`Soma: ${oppSum(num1, num2)}`);
+  console.log(`Diferenca ${oppSub(num1, num2)}`);
+  console.log(`Multiplicacao: ${oppMult(num1, num2)}`);
+  console.log(`Divisao: ${oppDiv(num1, num2)}`);
+};
+
+printAnswers(numberInput1, numberInput2);

a7e525bff7d7e8934761436bc4b9a0e3476475df
paimdev <bathkelucas@gmail.com>
2021-12-02 12:36:52 -0300

    Finish first coding exercise

--- modulo1/funcoes/template_javascript/index.js
+++ modulo1/funcoes/template_javascript/index.js
@@ -1,11 +1,11 @@
 // Exercicios
 // 1:
 function minhaFuncao(variavel) {
-	return variavel * 5
+  return variavel * 5;
 }
 
-console.log(minhaFuncao(2))
-console.log(minhaFuncao(10))
+console.log(minhaFuncao(2));
+console.log(minhaFuncao(10));
 
 // 10 e 50
 // nada
@@ -13,15 +13,24 @@ console.log(minhaFuncao(10))
 // 2:
 let textoDoUsuario = prompt("Insira um texto");
 
-const outraFuncao = function(texto) {
-	return texto.toLowerCase().includes("cenoura")
-}
+const outraFuncao = function (texto) {
+  return texto.toLowerCase().includes("cenoura");
+};
 
-const resposta = outraFuncao(textoDoUsuario)
-console.log(resposta)
+const resposta = outraFuncao(textoDoUsuario);
+console.log(resposta);
 
 // A function retorna o texto em minusculo e diz se existe a palavra cenoura.
 // i. eu gost de cenoura true
 // ii. cenoura e bom pra vista true
 // iii. cenouras crescem na terra true
 
+// Exercicios de codigo
+// 1:
+const autoGreeter = () =>
+  console.log("Eu sou Lucas, tenho 19 anos, moro em curitiba e sou estudante.");
+
+const greeter = (name, age, city, occupation) =>
+  console.log(
+    `Eu sou ${name}, tenho ${age} anos, moro em ${city} e sou ${occupation}.`
+  );
